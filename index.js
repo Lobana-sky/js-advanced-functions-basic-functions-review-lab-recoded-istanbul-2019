c5e401dc2b67a4bfda41902371f338b967035f74
lobana alarabi <loubanaeng@gmail.com>
2020-02-05 05:32:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+//---------
+function saturdayFun(pass="roller-skate"){
+  return `This Saturday, I want to ${pass}!`;
+ 
+}
+ console.log(saturdayFun());
+ 
+//----------
+let mondayWork=function(pass="go to the office")
+{
+  return `This Monday, I will ${pass}.`
+}
+mondayWork();
+//----------
